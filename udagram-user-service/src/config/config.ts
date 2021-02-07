3b797b808876747fc37f6081d40fdbbbe820c9ec
Javier Rey de Viñas <javier@iahorro-Javier.Rey>
2021-02-07 23:59:12 +0100

    ´refactor: Clean code - Remove the feeds part

---
+++ udagram-user-service/src/config/config.ts
@@ -0,0 +1,15 @@
+export const config = {
+  'username': 'dbadmin',
+  'password': 'JjdorA4dEt5Man_sdlF',
+  'database': 'postgres',
+  'host': "udagram-project4-microservices.cdnyj1kcpjyt.eu-west-1.rds.amazonaws.com",
+  'dialect': "postgres",
+  'aws_region': 'eu-west-1c',
+  'aws_profile': 'default',
+  // 'aws_region': process.env.AWS_REGION,
+  // 'aws_media_bucket': process.env.AWS_BUCKET,
+  // 'url': process.env.URL,
+  // 'jwt': {
+  //   'secret': process.env.JWT_SECRET,
+  // },
+};
