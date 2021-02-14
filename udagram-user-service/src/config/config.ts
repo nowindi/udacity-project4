a4134b4dcaceef4bc65ca5a90d2b9de3d518ba1e
Javier Rey de Viñas <javier@iahorro-Javier.Rey>
2021-02-14 18:14:43 +0100

    fix: env variables in config file

--- udagram-user-service/src/config/config.ts
+++ udagram-user-service/src/config/config.ts
@@ -1,16 +1,11 @@
 export const config = {
-  'username': 'dbadmin',
-  'password': 'JjdorA4dEt5Man_sdlF',
-  'database': 'postgres',
-  'host': "udagram-project4-microservices.cdnyj1kcpjyt.eu-west-1.rds.amazonaws.com",
+  'username': process.env.DB_USERNAME,
+  'password': process.env.DB_PASSWORD,
+  'database': process.env.DB_NAME,
+  'host': process.env.DB_HOST,
   'dialect': "postgres",
-  'aws_region': 'eu-west-1c',
-  'aws_profile': 'default',
-  'url': 'http://localhost:8080'
-  // 'aws_region': process.env.AWS_REGION,
-  // 'aws_media_bucket': process.env.AWS_BUCKET,
-  // 'url': process.env.URL,
-  // 'jwt': {
-  //   'secret': process.env.JWT_SECRET,
-  // },
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': 'http://localhost:8080',
 };
